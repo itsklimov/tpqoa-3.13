cbda9363f1b2151bf6f1856841a695223bd9e8f0
itsklimov <itsklimov@gmail.com>
2025-03-07 13:31:47 -0800

    updated package to work with python > 3.10

--- setup.py
+++ setup.py
@@ -7,20 +7,21 @@
 #
 from setuptools import setup
 
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
-setup(name='tpqoa',
-      version='0.0.56',
-      description='tpqoa Algorithmic Trading with Oanda',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author='Yves Hilpisch',
-      author_email='team@tpq.io',
-      url='http://home.tpq.io',
-      packages=['tpqoa'],
-      install_requires=[
-          'v20==3.0.25.0',
-          'pyyaml'
-      ]
-      )
+setup(
+    name="tpqoa",
+    version="0.0.57",
+    description="tpqoa Algorithmic Trading with Oanda updated to 3.13",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Yves Hilpisch",
+    author_email="team@tpq.io",
+    url="http://home.tpq.io",
+    packages=["tpqoa"],
+    install_requires=[
+        "v20 @ git+https://github.com/itsklimov/v20-python-3.13.git@5d2522c6d32b2dad1455d1ba3cafba39f4c1d23d",
+        "pyyaml",
+    ],
+)
